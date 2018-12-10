68b1e5e2100ea76b7174be3aafa1acce896cfe41
k.i.geiko <k.i.geiko@gmail.com>
2018-12-10 23:00:50 +0200

    Set apartment numbers

---
+++ js/setApartments.js
@@ -0,0 +1,121 @@
+var setApartmentNumbers = function(floorNumber){
+	if(floorNumber === 14){
+		$('.apartment1 > span').text(312);
+		$('.apartment2 > span').text(309);
+		$('.apartment3 > span').text(308);
+		$('.apartment4 > span').text(304);
+		$('.apartment5 > span').text(305);
+		$('.apartment6 > span').text(311);
+		$('.apartment7 > span').text(310);
+		$('.apartment8 > span').text(307);
+		$('.apartment9 > span').text(306);
+	}
+	else if(floorNumber === 13){
+		$('.apartment1 > span').text(303);
+		$('.apartment2 > span').text(299);
+		$('.apartment3 > span').text(300);
+		$('.apartment4 > span').text(302);
+		$('.apartment5 > span').text(301);
+	}
+	else if(floorNumber === 12){
+		$('.apartment1 > span').text(298);
+		$('.apartment2 > span').text(295);
+		$('.apartment3 > span').text(294);
+		$('.apartment4 > span').text(290);
+		$('.apartment5 > span').text(291);
+		$('.apartment6 > span').text(297);
+		$('.apartment7 > span').text(296);
+		$('.apartment8 > span').text(293);
+		$('.apartment9 > span').text(292);
+	}
+	else if(floorNumber === 11){
+		$('.apartment1 > span').text(289);
+		$('.apartment2 > span').text(285);
+		$('.apartment3 > span').text(286);
+		$('.apartment4 > span').text(288);
+		$('.apartment5 > span').text(287);
+	}
+	else if(floorNumber === 10){
+		$('.apartment1 > span').text(284);
+		$('.apartment2 > span').text(281);
+		$('.apartment3 > span').text(280);
+		$('.apartment4 > span').text(276);
+		$('.apartment5 > span').text(277);
+		$('.apartment6 > span').text(283);
+		$('.apartment7 > span').text(282);
+		$('.apartment8 > span').text(279);
+		$('.apartment9 > span').text(278);
+	}
+	else if(floorNumber === 9){
+		$('.apartment1 > span').text(275);
+		$('.apartment2 > span').text(271);
+		$('.apartment3 > span').text(272);
+		$('.apartment4 > span').text(274);
+		$('.apartment5 > span').text(273);
+	}
+	else if(floorNumber === 8){
+		$('.apartment1 > span').text(270);
+		$('.apartment2 > span').text(267);
+		$('.apartment3 > span').text(266);
+		$('.apartment4 > span').text(262);
+		$('.apartment5 > span').text(263);
+		$('.apartment6 > span').text(269);
+		$('.apartment7 > span').text(268);
+		$('.apartment8 > span').text(265);
+		$('.apartment9 > span').text(264);
+	}
+	else if(floorNumber === 7){
+		$('.apartment1 > span').text(261);
+		$('.apartment2 > span').text(257);
+		$('.apartment3 > span').text(258);
+		$('.apartment4 > span').text(260);
+		$('.apartment5 > span').text(259);
+	}
+	else if(floorNumber === 6){
+		$('.apartment1 > span').text(256);
+		$('.apartment2 > span').text(253);
+		$('.apartment3 > span').text(252);
+		$('.apartment4 > span').text(248);
+		$('.apartment5 > span').text(249);
+		$('.apartment6 > span').text(255);
+		$('.apartment7 > span').text(254);
+		$('.apartment8 > span').text(251);
+		$('.apartment9 > span').text(250);
+	}
+	else if(floorNumber === 5){
+		$('.apartment1 > span').text(247);
+		$('.apartment2 > span').text(243);
+		$('.apartment3 > span').text(244);
+		$('.apartment4 > span').text(246);
+		$('.apartment5 > span').text(245);
+	}
+	else if(floorNumber === 4){
+		$('.apartment1 > span').text(242);
+		$('.apartment2 > span').text(239);
+		$('.apartment3 > span').text(238);
+		$('.apartment4 > span').text(234);
+		$('.apartment5 > span').text(235);
+		$('.apartment6 > span').text(241);
+		$('.apartment7 > span').text(240);
+		$('.apartment8 > span').text(237);
+		$('.apartment9 > span').text(236);
+	}
+	else if(floorNumber === 3){
+		$('.apartment1 > span').text(233);
+		$('.apartment2 > span').text(229);
+		$('.apartment3 > span').text(230);
+		$('.apartment4 > span').text(232);
+		$('.apartment5 > span').text(231);
+	}
+	else if(floorNumber === 2){
+		$('.apartment1 > span').text(228);
+		$('.apartment2 > span').text(225);
+		$('.apartment3 > span').text(224);
+		$('.apartment4 > span').text(220);
+		$('.apartment5 > span').text(221);
+		$('.apartment6 > span').text(227);
+		$('.apartment7 > span').text(226);
+		$('.apartment8 > span').text(223);
+		$('.apartment9 > span').text(222);
+	}
+}
--- js/setFloor.js
+++ js/setFloor.js
@@ -1,75 +1,30 @@
 'use strict'
 $(document).ready(function(){	
-	$(document).on('click', 'div[id^="fa14"]', function() {
+
+	$(document).on('click', 'div[id^="fa14"]', function() {showFloor(14);});
+	$(document).on('click', 'div[id^="fa13"]', function() {showFloor(13);});
+	$(document).on('click', 'div[id^="fa12"]', function() {showFloor(12);});
+	$(document).on('click', 'div[id^="fa11"]', function() {showFloor(11);});
+	$(document).on('click', 'div[id^="fa10"]', function() {showFloor(10);});
+	$(document).on('click', 'div[id^="f9"]', function() {showFloor(9);});
+	$(document).on('click', 'div[id^="f8"]', function() {showFloor(8);});
+	$(document).on('click', 'div[id^="f7"]', function() {showFloor(7);});
+	$(document).on('click', 'div[id^="f6"]', function() {showFloor(6);});
+	$(document).on('click', 'div[id^="f5"]', function() {showFloor(5);});
+	$(document).on('click', 'div[id^="f4"]', function() {showFloor(4);});
+	$(document).on('click', 'div[id^="f3"]', function() {showFloor(3);});
+	$(document).on('click', 'div[id^="f2"]', function() {showFloor(2);});
+	$(document).on('click', 'div[id^="f1"]', function() {showFloor(1);});
+
+	var showFloor = function(fooorNumber){		
 		hideFloors();
-		$(".floor14").removeClass("hidden");
-		$("#fa14").css("background-color", "#1c3237")	
-	});
-	$(document).on('click', 'div[id^="fa13"]', function() {
-		hideFloors();
-		$(".floor13").removeClass("hidden");
-		$("#fa13").css("background-color", "#1c3237")	
-	});
-	$(document).on('click', 'div[id^="fa12"]', function() {
-		hideFloors();
-		$(".floor12").removeClass("hidden");
-		$("#fa12").css("background-color", "#1c3237")	
-	});
-	$(document).on('click', 'div[id^="fa11"]', function() {
-		hideFloors();
-		$(".floor11").removeClass("hidden");
-		$("#fa11").css("background-color", "#1c3237")	
-	});
-	$(document).on('click', 'div[id^="fa10"]', function() {
-		hideFloors();
-		$(".floor10").removeClass("hidden");
-		$("#fa10").css("background-color", "#1c3237")	
-	});
-	$(document).on('click', 'div[id^="f9"]', function() {
-		hideFloors();
-		$(".floor9").removeClass("hidden");
-		$("#f9").css("background-color", "#1c3237")	
-	});
-	$(document).on('click', 'div[id^="f8"]', function() {
-		hideFloors();
-		$(".floor8").removeClass("hidden");
-		$("#f8").css("background-color", "#1c3237")	
-	});
-	$(document).on('click', 'div[id^="f7"]', function() {
-		hideFloors();
-		$(".floor7").removeClass("hidden");
-		$("#f7").css("background-color", "#1c3237")	
-	});
-	$(document).on('click', 'div[id^="f6"]', function() {
-		hideFloors();
-		$(".floor6").removeClass("hidden");
-		$("#f6").css("background-color", "#1c3237")	
-	});
-	$(document).on('click', 'div[id^="f5"]', function() {
-		hideFloors();
-		$(".floor5").removeClass("hidden");
-		$("#f5").css("background-color", "#1c3237")	
-	});
-	$(document).on('click', 'div[id^="f4"]', function() {
-		hideFloors();
-		$(".floor4").removeClass("hidden");
-		$("#f4").css("background-color", "#1c3237")	
-	});
-	$(document).on('click', 'div[id^="f3"]', function() {
-		hideFloors();
-		$(".floor3").removeClass("hidden");
-		$("#f3").css("background-color", "#1c3237")	
-	});
-	$(document).on('click', 'div[id^="f2"]', function() {
-		hideFloors();
-		$(".floor2").removeClass("hidden");
-		$("#f2").css("background-color", "#1c3237")	
-	});
-	$(document).on('click', 'div[id^="f1"]', function() {
-		hideFloors();
-		$(".floor1").removeClass("hidden");
-		$("#f1").css("background-color", "#1c3237")	
-	});
+		$(".floor" + fooorNumber).removeClass("hidden");
+		if(fooorNumber >= 10)
+			$("#fa" + fooorNumber).css("background-color", "#1c3237");
+		else	
+			$("#f" + fooorNumber).css("background-color", "#1c3237");
+		setApartmentNumbers(fooorNumber);
+	}
 
 	var hideFloors = function(){
 		$(".floor14").addClass("hidden");
